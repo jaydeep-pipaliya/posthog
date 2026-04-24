c60f6625786e81fcb33a50121ca4091ece657a61
Yasen Slavov <yasen@posthog.com>
2026-04-24 10:08:54 +0300

    feat(notifications): add realtime_notifications_disabled prefs key

--- posthog/api/test/test_user.py
+++ posthog/api/test/test_user.py
@@ -1491,6 +1491,7 @@ class TestUserAPI(APIBaseTest):
                 "materialized_view_sync_failed": True,
                 "web_analytics_weekly_digest": True,
                 "organization_member_join_email_disabled": {},
+                "realtime_notifications_disabled": {},
             },
         )
 
@@ -1509,6 +1510,7 @@ class TestUserAPI(APIBaseTest):
                 "materialized_view_sync_failed": True,
                 "web_analytics_weekly_digest": True,
                 "organization_member_join_email_disabled": {},
+                "realtime_notifications_disabled": {},
             },
         )
 
@@ -1562,6 +1564,10 @@ class TestUserAPI(APIBaseTest):
             },
         )
 
+    def test_notification_settings_default_includes_realtime_disabled_empty_dict(self):
+        user = self._create_user("rt-defaults@test.com")
+        assert user.notification_settings["realtime_notifications_disabled"] == {}
+
     def test_invalid_notification_settings_returns_error(self):
         response = self.client.patch("/api/users/@me/", {"notification_settings": {"invalid_key": True}})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -1613,6 +1619,7 @@ class TestUserAPI(APIBaseTest):
                 "materialized_view_sync_failed": False,  # Default value
                 "web_analytics_weekly_digest": True,  # Default value
                 "organization_member_join_email_disabled": {},  # Default value
+                "realtime_notifications_disabled": {},  # Default value
             },
         )
 
